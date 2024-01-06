1fe402f28f5a5c3032389c6eacb25e162b1ebd4e
GabrielMayorgaRRar <142435908+GabrielMayorgaRRar@users.noreply.github.com>
2024-01-06 09:01:09 +0000

    Banco

---
+++ Banco.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Cliente
+{
+    char nombre_del_cliente[30];
+    int numero_del_cliente;
+    float saldo_actual;
+    bool estatus;
+};
+
+int main(){
+    do
+    {
+        /* code */
+    }while (/* condition */)
+    {
+        /* code */
+    }
+    
+    
+}
